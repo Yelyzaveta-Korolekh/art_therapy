1507f4e539d39d2e94ac7319058b8688fea2705a
Yelyzaveta-Korolekh <liza.koroleh@ukr.net>
2022-07-15 23:40:32 +0300

    scale pict

--- app/polls/views.py
+++ app/polls/views.py
@@ -5,6 +5,11 @@ from .models import Gallery
 
 def index(request):
     img_gall = Gallery.objects.all()
+    """  for i in img_gall:
+        if request.POST.get():
+            img = img_gall[i:i+2]
+        else:
+            break """
     img = img_gall[:2]
     return render(request, 'index.html', 
     {
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,6 +1,65 @@
 
 
-  
+
+function description(){
+    let target = event.target;
+    target.setAttribute("id","clickedImg");
+
+    gsap.timeline()
+    .to('.light', {
+        scale: 1,
+        duration:0.001,
+    })
+    .to('.sign', {
+        autoAlpha: 0,
+    })
+    .to('.img',  { 
+      autoAlpha: 0,
+    })
+    .to('#clickedImg', {
+      top: "+=10%",
+      autoAlpha: 1,
+      scale: 1.2,
+    })
+    .to('#text_img',  { 
+        autoAlpha: 0.9,
+      })
+    .to('#arrow', {
+        autoAlpha: 1,
+    })
+    
+    
+        
+}
+
+function mainPage(){
+    
+    gsap.timeline()
+    .to('.light', {
+        scale: 0.9,
+    })
+    .to('#arrow', {
+        autoAlpha: 0,
+    })
+    .to('#text_img',  { 
+        autoAlpha: 0,
+      })
+    .to('#clickedImg', {
+        top: "-=10%",
+        autoAlpha: 1,
+        scale: 1,
+    })
+    .to('.img',  { 
+        autoAlpha: 1,
+    })
+    .to('.sign', {
+        autoAlpha: 1,
+    })
+    
+    
+   
+      document.getElementById('clickedImg').removeAttribute('id');
+}
 
 
     
